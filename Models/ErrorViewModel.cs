8ff11a5d3589cbbc48e6bb5b7b17bbe218c1579e
DinhDat <1721050505@student.humg.edu.vn>
2022-10-06 07:34:08 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DinhTienDatTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
